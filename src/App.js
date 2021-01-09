1302b8b4545b5a63b58d30893f8b84fff0da544f
Rick <rmansfield@realpeoplesratings.com>
2021-01-09 17:33:40 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+import { CopyCatContainer } from './containers/CopyCatContainer';
+
+function App() {
+  return (
+    <CopyCatContainer />
+  );
+}
+
+export default App;
