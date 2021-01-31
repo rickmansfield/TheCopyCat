b9a996f215a46a79c020c988230bd85481ab6120
Rick <rmansfield@realpeoplesratings.com>
2021-01-31 11:46:21 -0600

    Updates

---
+++ precache-manifest.127d5995724377f1e4edab9f44456b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e24196f4b42f06f005f995cad4be26",
+    "url": "/TheCopyCat/index.html"
+  },
+  {
+    "revision": "478961760f79cade7f50",
+    "url": "/TheCopyCat/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e7deb41da876cb66e459",
+    "url": "/TheCopyCat/static/js/2.a7b9ccf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheCopyCat/static/js/2.a7b9ccf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478961760f79cade7f50",
+    "url": "/TheCopyCat/static/js/main.730a0b97.chunk.js"
+  },
+  {
+    "revision": "756b12121d894f05ef7a",
+    "url": "/TheCopyCat/static/js/runtime-main.6e00a357.js"
+  }
+]);
